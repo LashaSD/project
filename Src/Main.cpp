4459954bd99b90d7687c1b92a56f566fc768639f
LashaSD <trachenmacher@gmail.com>
2023-01-01 19:47:51 +0400

    Add Texture Support

--- Src/Main.cpp
+++ Src/Main.cpp
@@ -3,16 +3,21 @@
 #include <SDL_image.h>
 
 #include "RenderWindow.hpp"
+#include "Entity.hpp"
 
 int main(int argc, char* args[])
 {
-	if (SDL_Init(SDL_INIT_EVERYTHING) > 0)
+	if (SDL_Init(SDL_INIT_EVERYTHING) < 0)
 		printf("Couldn't Initialize SDL, SDL_ERROR: %s\n", SDL_GetError());
 
 	if (!(IMG_Init(IMG_INIT_PNG)))
 		printf("Couldn't initialize SDL_Image, SDL_ERROR %s\n", IMG_GetError());
 	
 	RenderWindow window("Program V1.0.0", 1280, 720);
+
+	SDL_Texture* grassTexture = window.loadTexture("Res/gfx/ground_grass.png");
+
+	Entity platform0(100, 100, grassTexture);
 
 	bool appRunning = true;
 	SDL_Event e;
@@ -24,6 +29,9 @@ int main(int argc, char* args[])
 			if (e.type == SDL_QUIT)
 				appRunning = false;
 		}
+		window.clear();
+		window.render(platform0);
+		window.display();
 	}
 	window.cleanUp();
 	SDL_Quit();
--- Src/RenderWindow.cpp
+++ Src/RenderWindow.cpp
@@ -3,6 +3,7 @@
 #include <iostream>
 
 #include "RenderWindow.hpp"
+#include "Entity.hpp"
 
 RenderWindow::RenderWindow(const char* p_title, int p_w, int p_h)
 	:window(NULL), renderer(NULL)
@@ -13,7 +14,44 @@ RenderWindow::RenderWindow(const char* p_title, int p_w, int p_h)
 	renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
 }
 
+SDL_Texture* RenderWindow::loadTexture(const char* p_filePath)
+{
+	SDL_Texture* texture = nullptr;
+	texture = IMG_LoadTexture(renderer, p_filePath);
+
+	if (texture == nullptr)
+		printf("Couldn't load the Texture, IMG_ERROR: %s\n", IMG_GetError());	
+
+	return texture;
+}
+
 void RenderWindow::cleanUp()
 {
 	SDL_DestroyWindow(window);
+}
+
+void RenderWindow::clear()
+{
+	SDL_RenderClear(renderer);
+}
+
+void RenderWindow::render(Entity& p_entity)
+{
+	SDL_Rect src; //Starting Point of render of the Texture
+	src.x = p_entity.getCurrentFrame().x;
+	src.y = p_entity.getCurrentFrame().y;
+	src.w = p_entity.getCurrentFrame().w;
+	src.h = p_entity.getCurrentFrame().h;
+
+	SDL_Rect dest; //Finishing Point of render of the Texture
+	dest.x = p_entity.getX();
+	dest.y = p_entity.getY();
+	dest.w = p_entity.getCurrentFrame().w * 2;
+	dest.h = p_entity.getCurrentFrame().h * 2;	 
+	SDL_RenderCopy(renderer, p_entity.getTexture(), &src, &dest);
+}
+
+void RenderWindow::display()
+{
+	SDL_RenderPresent(renderer);
 }
