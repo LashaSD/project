c8979df330606a88985f7e483fa452c6852d6f3a
LashaSD <trachenmacher@gmail.com>
2023-01-06 23:13:23 +0400

    add frame limit

--- Src/Main.cpp
+++ Src/Main.cpp
@@ -5,6 +5,7 @@
 
 #include "RenderWindow.hpp"
 #include "Entity.hpp"
+#include "RigidBody.hpp"
 #include "Math.hpp"
 #include "Utils.hpp"
 
@@ -23,19 +24,24 @@ int main(int argc, char* args[])
 
 	SDL_Texture* grassTexture = window.loadTexture("Res/gfx/ground_grass.png");
 
-	std::vector<Entity> entities = {Entity(Vector2f(0, 96),grassTexture),
-									Entity(Vector2f(32, 96),grassTexture),
-									Entity(Vector2f(64, 96),grassTexture),
-									Entity(Vector2f(96, 96),grassTexture),
-									Entity(Vector2f(128, 96),grassTexture),};
+	Rigidbody rb(Vector2f(0, 96), grassTexture, 5.0f);
+	rb.setVelocity(1.0f, 1.0f);
 
-	window.addToQueue(entities);
+	std::vector<Rigidbody> entities = { rb };
+
+	window.addToScene(entities);
 
 	bool appRunning = true;
 	SDL_Event e;
 
+	float beginTicks = SDL_GetTicks();
+	int frames = 0;
+
 	while (appRunning)
 	{
+
+		frames++;
+		float start = utils::hireTimeInSeconds();
 
 		window.clear();
 
@@ -50,27 +56,34 @@ int main(int argc, char* args[])
 
 		// Physics Loop
 
-		for (Entity& entity : window.getQueue())
+		for (auto& entity : window.getScene())
 		{
-			entity.getPos().x += entity.getAcceleration().x;
-			entity.getPos().y += entity.getAcceleration().y;
+			Vector2f oldVelocity = entity.getVelocity();
 			if (entity.getPos().x >= 1248 || entity.getPos().x <= 0)
 			{
 				// Collision with the Wall on the X-Axis
-				entity.accelerate(((entity.getAcceleration().x)*-2), 0);
-				entity.getAcceleration().x -= 1.0f;
+				entity.setVelocity(-(oldVelocity.x), oldVelocity.y);
+				
 			}
 			if (entity.getPos().y >= 688 || entity.getPos().y <= 0)
 			{
-				// Collision with the Wall on the Y-Axis
-				entity.accelerate(0, ((entity.getAcceleration().y)*-2));
-				entity.getAcceleration().y += 1.0f;
+				// Collision with the Roof on the Y-Axis
+				entity.setVelocity(oldVelocity.x, -(oldVelocity.y));
 			}
+			float dt = utils::hireTimeInSeconds() - start;
+			entity.updatePhysics(dt);
 		}
 
 		// Render Loop
-		window.renderQueue();
+		window.renderScene();
 		window.display();
+
+		float elapsedMs = utils::hireTimeInSeconds() - start;
+
+		if (elapsedMs < 0.1666)
+			SDL_Delay(16 - elapsedMs);
+
+		// printf("FPS: %f\n", (float)frames / ((SDL_GetTicks() - beginTicks) / 1000));
 	}
 	window.cleanUp();
 	SDL_Quit();
