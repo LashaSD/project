6ab2166a164d48b77ff408b76c8a095a137d6d64
LashaSD <trachenmacher@gmail.com>
2023-01-06 23:12:56 +0400

    add vector2 class

--- Include/Math.hpp
+++ Include/Math.hpp
@@ -17,3 +17,21 @@ struct Vector2f
 
 	float x, y;
 };
+
+struct Vector2 
+{
+	Vector2() 
+	:x(0), y(0)
+	{ }
+
+	Vector2(int p_x, int p_y) 
+	:x(p_x), y(p_y)
+	{ }
+
+	void print()
+	{
+		printf("x: %d, y: %d\n", x, y);
+	}
+
+	int x, y;
+};
